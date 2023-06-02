e50d78b9ae20f19b93fe4aec129291075a67e61d
anjali29singh <anjalisingh930260@gmail.com>
2023-06-02 16:15:25 +0530

    useState-hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import { createRoot } from "react-dom";
-import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 //without jsx
 // const Pet = (props) => {
@@ -11,13 +12,18 @@ import Pet from "./Pet";
 //     React.createElement("h3", {}, props.breed)
 //   );
 // };
+//whenever an event happens react re renders everything
 
 const App = () => {
-  <div>
-    <Pet name="bruno" animal="dog" breed="husky" />
-    <Pet name="bunny" animal="rabbit" breed="rabbit" />
-    <Pet name="pepper" animal="bird" breed="Indo" />
-  </div>;
+  return (
+    <div>
+      <h1>Adopt-Me</h1>
+      <SearchParams />
+      {/* <Pet name="bruno" animal="dog" breed="husky" />
+      <Pet name="bunny" animal="rabbit" breed="rabbit" />
+      <Pet name="pepper" animal="bird" breed="Indo" /> */}
+    </div>
+  );
 };
 const container = document.getElementById("root");
 const root = createRoot(container);
