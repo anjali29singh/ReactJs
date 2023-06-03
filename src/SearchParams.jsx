d2f2f5a030cbdcd2146865a02c4edceb2fde9dc6
anjali29singh <anjalisingh930260@gmail.com>
2023-06-03 18:42:58 +0530

    useEffects

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -3,16 +3,32 @@ import { useState, useEffect } from "react";
 //this is controlled by external system
 
 const ANIMALS = ["dog", "cat", "rabbit", "bird", "panda"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
+
+  //useEffect(function,[dependencies]) //if not dependencies is given then useEffect is called when any time hook changes
+  //which we don't want
+  //useEffect is used to do somethings after render is done.
+  //this will called anytime whenver a hook is changed
+  /*useEffect(() => {
+    console.log("anjali");
+  });
+ */
+
+  useEffect(() => {
+    console.log("useEffect is called after animal state is changed");
+  }, [animal, location]);
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
           <input
-            onChange={(e) => setLocation(e.target.value)}
+            onChange={(e) => {
+              setLocation(e.target.value);
+            }}
             value={location}
             id="location"
             placeholder="location"
@@ -26,13 +42,13 @@ const SearchParams = () => {
             id="animals"
             value={animal}
             onChange={(e) => {
+              e.preventDefault();
               setAnimal(e.target.value);
             }}
           >
             <option />
             {ANIMALS.map((animal) => {
-              return;
-              <option key={animal}>{animal}</option>;
+              return <option key={animal}>{animal}</option>;
             })}
           </select>
         </label>
